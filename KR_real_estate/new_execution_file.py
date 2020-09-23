6657299ba4658c8146af94adee6e8a787999b17d
Devin <34824927+devhkim@users.noreply.github.com>
2020-09-23 15:50:11 +0200

    Update new_execution_file.py

--- KR_real_estate/new_execution_file.py
+++ KR_real_estate/new_execution_file.py
@@ -13,6 +13,7 @@ apt_price = apt.APT()
 
 #import the dictionary via the method
 #to use local, type in 0 for dbname (5th parameter)
+# get the "xxx" values from Google Keep
 apt_price.get_to_mdb(1, 2015, 8, 2020,
                    "C:\\Users\\Devin\\Downloads\\reg_code_test.xlsx",
                    "xxx",
